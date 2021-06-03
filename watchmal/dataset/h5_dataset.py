37616fbdf5c924c2e2ff998e8e273d670aac2efc
Joanna-Gao <joannagao6918@gmail.com>
2021-06-03 02:45:14 -0700

    Updated h5 dataset code and pointnet data object code to accommodate hybrid hk geometry

--- watchmal/dataset/h5_dataset.py
+++ watchmal/dataset/h5_dataset.py
@@ -12,21 +12,34 @@ from abc import ABC, abstractmethod
 
 class H5CommonDataset(Dataset, ABC):
     """
-    Initialize with file of h5 data.  Sets up access to all of the data that is common between
-    the digitized hits data and the truth hits data.  These are:
+    Initialize with file of h5 data.  Sets up access to all of the data that is
+    common between the digitized hits data and the truth hits data.  These are:
     
     event_ids 	(n_events,) 	int32 	ID of the event in the ROOT file
     root_files 	(n_events,) 	object 	File name and location of the ROOT file
-    labels 	(n_events,) 	int32 	Label for event classification (gamma=0, electron=1, muon=2)
-    positions 	(n_events,1,3) 	float32 	Initial (x, y, z) position of simulated particle
-    angles 	(n_events,2) 	float32 	Initial direction of simulated particle as (polar, azimuth) angles
-    energies 	(n_events,1) 	float32 	Initial total energy of simulated particle
-    veto 	(n_events,) 	bool 	OD veto estimate based on any Cherenkov producing particles exiting the tank, with initial energy above threshold
-    veto2 	(n_events,) 	bool 	OD veto estimate based on any Cherenkov producing particles exiting the tank, with an estimate of energy at the point the particle exits the tank being above threshold
-    event_hits_index 	(n_events,) 	int64 	Starting index in the hit dataset objects for hits of a particular event
+    labels 	    (n_events,) 	int32 	Label for event classification 
+                                      (gamma=0, electron=1, muon=2)
+    positions 	(n_events,1,3) 	float32 	Initial (x, y, z) position of 
+                                          simulated particle
+    angles 	    (n_events,2) 	float32 Initial direction of simulated particle as
+                                      (polar, azimuth) angles
+    energies 	  (n_events,1) 	float32	Initial total energy of simulated particle
+    veto 	      (n_events,) 	bool 	  OD veto estimate based on any Cherenkov 
+                                      producing particles exiting the tank, with
+                                      initial energy above threshold
+    veto2     	(n_events,) 	bool 	  OD veto estimate based on any Cherenkov 
+                                      producing particles exiting the tank, with
+                                      an estimate of energy at the point the
+                                      particle exits the tank being above 
+                                      threshold
+    event_hits_index 	(n_events,)   int64  Starting index in the hit dataset 
+                                           objects for hits of a particular event
     
-    hit_pmt 	(n_hits,) 	int32 	PMT ID of the digitized hit
-    hit_time 	(n_hits,) 	float32 	Time of the digitized hit
+    hit_pmt 	  (n_hits,) 	  int32 	  PMT ID of the digitized hit
+    hit_time 	  (n_hits,) 	  float32 	Time of the digitized hit
+
+    Note: event_hits_index, hit_pmt and hit_time have to be separate for 20" 
+          and 3" PMTs
     """
     def __init__(self, h5_path, is_distributed):
         """
@@ -56,25 +69,42 @@ class H5CommonDataset(Dataset, ABC):
         if "veto" in self.h5_file.keys():
             self.veto  = np.array(self.h5_file["veto"])
             self.veto2 = np.array(self.h5_file["veto2"])
-        self.event_hits_index = np.append(self.h5_file["event_hits_index"], self.h5_file["hit_pmt"].shape[0]).astype(np.int64)
-        
-        self.hdf5_hit_pmt  = self.h5_file["hit_pmt"]
-        self.hdf5_hit_time = self.h5_file["hit_time"]
+        self.event_hits_index_20 = np.append(self.h5_file["event_hits_index_20"],\
+            self.h5_file["hit_pmt_20"].shape[0]).astype(np.int64)  # why append this? FIXME!
+        self.event_hits_index_3 = np.append(self.h5_file["event_hits_index_3"],\
+            self.h5_file["hit_pmt_3"].shape[0]).astype(np.int64)
 
-        self.hit_pmt = np.memmap(self.h5_path, mode="r", shape=self.hdf5_hit_pmt.shape,
-                                 offset=self.hdf5_hit_pmt.id.get_offset(),
-                                 dtype=self.hdf5_hit_pmt.dtype)
+        self.hdf5_hit_pmt_20  = self.h5_file["hit_pmt_20"]
+        self.hdf5_hit_time_20 = self.h5_file["hit_time_20"]
 
-        self.time = np.memmap(self.h5_path, mode="r", shape=self.hdf5_hit_time.shape,
-                              offset=self.hdf5_hit_time.id.get_offset(),
-                              dtype=self.hdf5_hit_time.dtype)
+        self.hdf5_hit_pmt_3  = self.h5_file["hit_pmt_3"]
+        self.hdf5_hit_time_3 = self.h5_file["hit_time_3"]
+
+        self.hit_pmt_20 = np.memmap(self.h5_path, mode="r", 
+                                    shape=self.hdf5_hit_pmt_20.shape,
+                                    offset=self.hdf5_hit_pmt_20.id.get_offset(),
+                                    dtype=self.hdf5_hit_pmt_20.dtype)
+        self.hit_pmt_3 = np.memmap(self.h5_path, mode="r", 
+                                   shape=self.hdf5_hit_pmt_3.shape,
+                                   offset=self.hdf5_hit_pmt_3.id.get_offset(),
+                                   dtype=self.hdf5_hit_pmt_3.dtype)
+
+        self.time_20 = np.memmap(self.h5_path, mode="r", 
+                                 shape=self.hdf5_hit_time_20.shape,
+                                 offset=self.hdf5_hit_time_20.id.get_offset(),
+                                 dtype=self.hdf5_hit_time_20.dtype)
+        self.time_3 = np.memmap(self.h5_path, mode="r", 
+                                shape=self.hdf5_hit_time_3.shape,
+                                offset=self.hdf5_hit_time_3.id.get_offset(),
+                                dtype=self.hdf5_hit_time_3.dtype)
         self.load_hits()
 
         # Set attribute so that method won't be invoked again
         self.initialized = True
         
     @abstractmethod
-    def load_hits(self):
+    def load_hits(self):  
+        # place holder, this method awaits to be redefined by the subclasses
         pass
 
     def __len__(self):
@@ -99,34 +129,55 @@ class H5CommonDataset(Dataset, ABC):
 
 class H5Dataset(H5CommonDataset, ABC):
     """
-    Initialize digihits dataset.  Adds access to digitized hits data.  These are:
+    Initialize digihits dataset.  Adds access to digitized hits data. These are:
     hit_charge 	(n_hits,) 	float32 	Charge of the digitized hit
+    
+    Note: hit_charge has to be separate for 20" and 3" PMTs
     """
     def __init__(self, h5_path, is_distributed):
         H5CommonDataset.__init__(self, h5_path, is_distributed)
         
     def load_hits(self):
-        self.hdf5_hit_charge = self.h5_file["hit_charge"]
-        self.hit_charge = np.memmap(self.h5_path, mode="r", shape=self.hdf5_hit_charge.shape,
-                                    offset=self.hdf5_hit_charge.id.get_offset(),
-                                    dtype=self.hdf5_hit_charge.dtype)
+        self.hdf5_hit_charge_20 = self.h5_file["hit_charge_20"]
+        self.hit_charge_20 = np.memmap(self.h5_path, mode="r",
+                                       shape=self.hdf5_hit_charge_20.shape,
+                                       offset=self.hdf5_hit_charge_20.id.get_offset(),
+                                       dtype=self.hdf5_hit_charge_20.dtype)
+        self.hdf5_hit_charge_3 = self.h5_file["hit_charge_3"]
+        self.hit_charge_3 = np.memmap(self.h5_path, mode="r",
+                                      shape=self.hdf5_hit_charge_3.shape,
+                                      offset=self.hdf5_hit_charge_3.id.get_offset(),
+                                      dtype=self.hdf5_hit_charge_3.dtype)
         
     def __getitem__(self, item):
         data_dict = super().__getitem__(item)
 
-        start = self.event_hits_index[item]
-        stop = self.event_hits_index[item + 1]
+        start_20 = self.event_hits_index_20[item]
+        stop_20 = self.event_hits_index_20[item + 1]
 
-        self.event_hit_pmts = self.hit_pmt[start:stop].astype(np.int16)
-        self.event_hit_charges = self.hit_charge[start:stop]
-        self.event_hit_times = self.time[start:stop]
+        self.event_hit_pmts_20 = self.hit_pmt_20[start_20:stop_20].astype(np.int16)
+        self.event_hit_charges_20 = self.hit_charge_20[start_20:stop_20]
+        self.event_hit_times_20 = self.time_20[start_20:stop_20]
+
+        start_3 = self.event_hits_index_3[item]
+        stop_3 = self.event_hits_index_3[item + 1]
+
+        self.event_hit_pmts_3 = self.hit_pmt_3[start_3:stop_3].astype(np.int16)
+        self.event_hit_charges_3 = self.hit_charge_3[start_3:stop_3]
+        self.event_hit_times_3 = self.time_3[start_3:stop_3]
 
         return data_dict
 
 class H5TrueDataset(H5CommonDataset, ABC):
     """
-    Initializes truehits dataset. Adds access to true photon hits data. These are:
-    hit_parent 	(n_hits,) 	float32 	Parent track ID of the true hit, as defined by WCSim's true hit parent. -1 is used for dark noise.
+    Initializes truehits dataset. Adds access to true photon hits data. 
+    These are:
+    hit_parent 	  (n_hits,) 	float32 	Parent track ID of the true hit, as 
+                                        defined by WCSim's true hit parent. 
+                                        -1 is used for dark noise.
+
+    Note: the modification for hybrid HK geometry is not complete. Caution is
+          needed.
     """
     def __init__(self, h5_path, transforms=None, digitize_hits=True):
         H5CommonDataset.__init__(self, h5_path, transforms)
@@ -155,18 +206,18 @@ class H5TrueDataset(H5CommonDataset, ABC):
     def __getitem__(self, item):
         data_dict = super().__getitem__(item)
 
-        start = self.event_hits_index[item]
-        stop = self.event_hits_index[item + 1]
+        start_20 = self.event_hits_index_20[item]
+        stop_20 = self.event_hits_index_20[item + 1]
 
-        true_pmts    = self.hit_pmt[start:stop].astype(np.int16)
-        true_times   = self.time[start:stop]
-        true_parents = self.hit_parent[start:stop]
+        true_pmts    = self.hit_pmt_20[start_20:stop_20].astype(np.int16)
+        true_times   = self.time_20[start_20:stop_20]
+        true_parents = self.hit_parent[start_20:stop_20]
 
         if self.digitize_hits:
-            self.event_hit_pmts, self.event_hit_times, self.event_hit_charges = self.digitize(true_pmts, true_times, true_parents)
+            self.event_hit_pmts_20, self.event_hit_times_20, self.event_hit_charges_20 = self.digitize(true_pmts, true_times, true_parents)
         else:
-            self.event_hit_pmts = true_pmts
-            self.event_hit_times = true_times
+            self.event_hit_pmts_20 = true_pmts
+            self.event_hit_times_20 = true_times
             self.event_hit_parents = true_parents
 
         return data_dict
--- watchmal/dataset/pointnet/pointnet_dataset.py
+++ watchmal/dataset/pointnet/pointnet_dataset.py
@@ -15,31 +15,48 @@ import watchmal.dataset.data_utils as du
 
 class PointNetDataset(H5Dataset):
 
-    def __init__(self, h5file, geometry_file, is_distributed, use_orientations=False, n_points=4000, transforms=None):
+    def __init__(self, h5file, geometry_file, is_distributed, 
+                 use_orientations=False, n_points_20=4000, n_points_3=4000, 
+                 transforms=None):
+        """
+        Using the separate geo info format for the hybrid geometry
+        The 'n_points' value might need to change FIXME!! 
+        """
         super().__init__(h5file, is_distributed)
         geo_file = np.load(geometry_file, 'r')
-        self.geo_positions = torch.from_numpy(geo_file["position"]).float()
-        self.geo_orientations = torch.from_numpy(geo_file["orientation"]).float()
+        self.geo_positions_20 = torch.from_numpy(geo_file["position_20"]).float()
+        self.geo_orientations_20 = torch.from_numpy(geo_file["orientation_20"]).float()
+        self.geo_positions_3 = torch.from_numpy(geo_file["position_3"]).float()
+        self.geo_orientations_3 = torch.from_numpy(geo_file["orientation_3"]).float()
         self.use_orientations = use_orientations
-        self.n_points = n_points
+        self.n_points_20 = n_points_20
+        self.n_points_3 = n_points_3
         self.transforms = du.get_transformations(transformations, transforms)
-
 
     def  __getitem__(self, item):
 
         data_dict = super().__getitem__(item)
 
-        hit_positions = self.geo_positions[self.event_hit_pmts, :]
-        n_hits = min(self.n_points, self.event_hit_pmts.shape[0])
+        hit_positions_20 = self.geo_positions_20[self.event_hit_pmts_20, :]
+        n_hits_20 = min(self.n_points_20, self.event_hit_pmts_20.shape[0])
+        hit_positions_3 = self.geo_positions_3[self.event_hit_pmts_3, :]
+        n_hits_3 = min(self.n_points_3, self.event_hit_pmts_3.shape[0])
         if not self.use_orientations:
-            data = np.zeros((5, self.n_points))
+            data = np.zeros((5, self.n_points_20+self.n_points_3))  # number correct?? FIXME
         else:
-            hit_orientations = self.geo_orientations[self.event_hit_pmts[:n_hits], :]
-            data = np.zeros((7, self.n_points))
-            data[3:5, :n_hits] = hit_orientations.T
-        data[:3, :n_hits] = hit_positions[:n_hits].T
-        data[-2, :n_hits] = self.event_hit_charges[:n_hits]
-        data[-1, :n_hits] = self.event_hit_times[:n_hits]
+            hit_orientations_20 = self.geo_orientations_20[self.event_hit_pmts_20[:n_hits_20], :]
+            hit_orientations_3 = self.geo_orientations_3[self.event_hit_pmts_3[:n_hits_3], :]
+            data = np.zeros((7, self.n_points_20+self.n_points_3))  # number correct?? FIXME
+            data[3:5, :n_hits_20] = hit_orientations_20.T
+            data[3:5, self.n_points_20:n_hits_3] = hit_orientations_3.T  # 3"
+        # 20"
+        data[:3, :n_hits_20] = hit_positions_20[:n_hits_20].T
+        data[-2, :n_hits_20] = self.event_hit_charges_20[:n_hits_20]
+        data[-1, :n_hits_20] = self.event_hit_times_20[:n_hits_20]
+        # 3"
+        data[:3, self.n_points_20:n_hits_3] = hit_positions_3[:n_hits_3].T
+        data[-2, self.n_points_20:n_hits_3] = self.event_hit_charges_3[:n_hits_3]
+        data[-1, self.n_points_20:n_hits_3] = self.event_hit_times_3[:n_hits_3]
 
         data = du.apply_random_transformations(self.transforms, data)
 
